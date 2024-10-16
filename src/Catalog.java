ce366db5b433eecf71a49eedd187b9654dbf2e4a
ElinFoUni <3ggapp24.tvinel@folkuniversitetet.nu>
2024-10-16 10:12:32 +0200

    Catalog class

---
+++ src/Catalog.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Catalog {
+    ArrayList<String> products = new ArrayList<String>;
+
+    public void addProduct(Product name) {
+        products.add(name);
+    }
+
+
+
+
+}
